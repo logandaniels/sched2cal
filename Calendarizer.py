65b89e6b37ba78432f5fb2edceebc68bd92f65d6
Logan J Daniels <danie855@umn.edu>
2014-09-01 23:56:59 -0500

    Adding readme.

--- Calendarizer.py
+++ Calendarizer.py
@@ -1,6 +1,16 @@
 from icalendar import Calendar, Event
 
+CLASSES_START_DATE = "20140902"
+CLASSES_END_DATE =  "20150508"
+
 class Calendarizer:
     def __init__(self, courses):
         self.courses = courses
         cal = Calendar()
+
+    def getEvents():
+        for course in self.courses:
+            event = Event()
+            event.name = course.getTitle()
+            event.begin
+
