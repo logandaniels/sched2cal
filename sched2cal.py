0db967f7d30bfed80c5a85747173eb1dcc461cc8
Logan J Daniels <danie855@umn.edu>
2014-09-02 22:15:56 -0500

    Added author information.

--- sched2cal.py
+++ sched2cal.py
@@ -1,3 +1,13 @@
+# This script was created by Logan Daniels.
+# By using this script you assume full responsibility
+# for any consequences or damages resulting from its use.
+#
+# USAGE: "python sched2cal.py <html filename>"
+#
+# Upon first running this script, a browser window will
+# prompt you to grant the application access to your calendars.
+
+
 import CourseScraper
 import Calendarizer
 import Course
